2bf2592c5bbe2952be3dd9273806dbe6d0805603
KAIF KHAN <kaif8700979251@gmail.com>
2025-01-26 21:41:26 +0530

    Update videoFunction.js

--- videoFunction.js
+++ videoFunction.js
@@ -4,23 +4,28 @@ const generateVideo = async (videoDetails, audioPath, transcriptionDetails) => {
   try {
     const first = "./3571264-hd_1280_720_30fps.mp4"
     const second = "./3571264-hd_1280_720_30fps.mp4"
-    const third = "./3571264-hd_1280_720_30fps.mp4"
-    // Step 1: Create the concat file for videos
-    // const concatContent = videoDetails
-    //   .map((video) => `file '${video.path}'`)
-    //   .join("\n");
-    // await fs.writeFile(tempVideoList, concatContent);
+    const third = "./3571264-hd_1280_720_30fps.mp4
 
-    // // Step 2: Generate transcription subtitles (SRT file)
-    // const subtitleContent = transcriptionDetails
-    //   .map(
-    //     (item, index) =>
-    //       `${index + 1}\n${item.start} --> ${item.end}\n${item.text}\n\n`
-    //   )
-    //   .join("");
-    // await fs.writeFile(tempSubtitles, subtitleContent);
 
-    // Step 3: Combine videos into one
+
+
+    
+    const concatContent = videoDetails
+      .map((video) => `file '${video.path}'`)
+      .join("\n");
+    await fs.writeFile(tempVideoList, concatContent);
+
+
+    
+    const subtitleContent = transcriptionDetails
+      .map(
+        (item, index) =>
+          `${index + 1}\n${item.start} --> ${item.end}\n${item.text}\n\n`
+      )
+      .join("");
+    await fs.writeFile(tempSubtitles, subtitleContent);
+
+    
     await new Promise((resolve, reject) => {
       ffmpeg({source: first})
         .input(second)
@@ -31,24 +36,22 @@ const generateVideo = async (videoDetails, audioPath, transcriptionDetails) => {
         .mergeToFile('mergeVideo.mp4')
     });
 
-    // Step 4: Add audio and subtitles
-    // await new Promise((resolve, reject) => {
-    //   ffmpeg(tempVideoPath)
-    //     .input(audioPath)
-    //     .input(tempSubtitles)
-    //     .outputOptions("-c:v copy", "-c:a aac", "-c:s mov_text")
-    //     .outputOptions("-shortest")
-    //     .save(outputPath)
-    //     .on("end", resolve)
-    //     .on("error", reject);
-    // });
+    await new Promise((resolve, reject) => {
+      ffmpeg(tempVideoPath)
+        .input(audioPath)
+        .input(tempSubtitles)
+        .outputOptions("-c:v copy", "-c:a aac", "-c:s mov_text")
+        .outputOptions("-shortest")
+        .save(outputPath)
+        .on("end", resolve)
+        .on("error", reject);
+    });
 
-    // // Clean up temporary files
-    // await fs.remove(tempVideoList);
-    // await fs.remove(tempSubtitles);
-    // await fs.remove(tempVideoPath);
+    await fs.remove(tempVideoList);
+    await fs.remove(tempSubtitles);
+    await fs.remove(tempVideoPath);
 
-    return "outputPath";
+    return outputPath;
   } catch (error) {
     console.error("Error generating video:", error);
   }
